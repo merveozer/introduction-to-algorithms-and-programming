ed646d6b1ffbe3f4b90aca238005437f4a77dd99
merveozer <74708604+merveozer@users.noreply.github.com>
2020-12-07 15:15:53 +0300

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a[20];
+	a[0]=1;
+	a[1]=1;
+	for (int i=2; i<20; i++){
+
+	    a[i]=a[i-1]+a[i-2];
+	    
+	    printf("%d\n", a[i]);
+	}
+	
+	
+	
+	return 0;
+}
