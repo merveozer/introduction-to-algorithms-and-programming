6983543f26973025282e05c64319b985df4ae42f
merveozer <74708604+merveozer@users.noreply.github.com>
2020-12-07 14:32:04 +0300

    Add files via upload

---
+++ kaç adet a.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char cumle[100];
+	printf("bir cumle giriniz: ");
+	gets(cumle);
+	int sayac=0;
+	for (int i=0; i<strlen(cumle);i++)
+	if(cumle[i]== a)
+	sayac++;
+	printf("girilen cumlede %d tane a harfi vardir", sayac);
+	
+	
+	return 0;
+}
